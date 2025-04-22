8b9d8f86757349f189f4b55bb259c2deeee62cf6
Muhammadazim <shuhrataliyevm@gmail.com>
2025-04-22 19:03:11 +0500

    m

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ import LibraryList from './pages/LibraryList/libraryList';
 const queryClient = new QueryClient();
 
 const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem('access_token');
     if (!token) {
         return <Navigate to="/login" />;
     }
